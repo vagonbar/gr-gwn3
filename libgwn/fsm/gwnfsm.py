c68d9127fc83a6b37856cc825292382a3dca4170
Víctor González Barbone <vgonzalezbarbone@gmail.com>
2021-09-08 20:59:08 -0300

    added COPYING file, updated LICENSE and license header in files

--- libgwn/fsm/gwnfsm.py
+++ libgwn/fsm/gwnfsm.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+# 
+# Copyright 2015-2021
+#    Instituto de Ingenieria Electrica, Facultad de Ingenieria,
+#    Universidad de la Republica, Uruguay.
+#    https://iie.fing.edu.uy/
 #
-#    Copyright (C) 2014-2021 by 
-#        Instituto de Ingeniería Eléctrica, 
-#        Facultad de Ingenieria, Universidad de la Republica, Uruguay.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    GNUWiNetwork is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    GNUWiNetwork is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See# the
-#    GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with GNUWiNetwork.  If not, see <http://www.gnu.org/licenses/>.
-#
+
+
 
 '''A Finite State Machine (FSM) engine.
 
--- python/__init__.py
+++ python/__init__.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+# 
+# Copyright 2015-2021
+#    Instituto de Ingenieria Electrica, Facultad de Ingenieria,
+#    Universidad de la Republica, Uruguay.
+#    https://iie.fing.edu.uy/
 #
-# Copyright 2008,2009 Free Software Foundation, Inc.
-#
-# This application is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This application is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+
 
 # The presence of this file turns this directory into a Python package
 
